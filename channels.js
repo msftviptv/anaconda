d4903d53be854c7c36212640b9f2d1c53e372385
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-02-09 16:30:21 +0200

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,4 +1,4 @@
 var beinsport1low = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/19.m3u8";
-var beinsport1sd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/11.m3u8";
-var beinsport1hd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/1.m3u8";
-var beinsport1fhd = "http://s1.mainevents.icu:8000/live/239021AM4884820/47212021AM4784345/1436.m3u8";
+    beinsport1sd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/11.m3u8";
+    beinsport1hd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/1.m3u8";
+    beinsport1fhd = "http://s1.mainevents.icu:8000/live/239021AM4884820/47212021AM4784345/1436.m3u8";
