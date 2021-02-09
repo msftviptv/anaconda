b8d8009f6884eec6922cf625211c8f23259c2e4f
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-02-09 11:38:40 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 var first0 = 'http://163.172.215.26:25461/live/';
-
+var first1 = 'http://s1.mainevents.icu:8000/live/';
 // vttpi اليوزر نيم بتاع موقع 
 
 var username0 = 'BEST-LIVE1177';
@@ -8,3 +8,12 @@ var username0 = 'BEST-LIVE1177';
 // vttpi الباسورد بتاع موقع 
 
 var password0 = 'BEST-LIVE1177';
+
+
+
+var username1 = '239021AM4884820';
+
+
+// vttpi الباسورد بتاع موقع 
+
+var password1 = '47212021AM4784345';
