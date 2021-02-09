6ca415dce0b829e33e48cd55b64c7c21808ebbfa
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-02-09 11:52:36 +0200

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,1 +1,3 @@
-var beinsport1 = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/19.m3u8";
+var beinsport1low = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/19.m3u8";
+var beinsport1sd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/11.m3u8";
+var beinsport1hd = "http://163.172.215.26:25461/live/BEST-LIVE1177/BEST-LIVE1177/1.m3u8";
