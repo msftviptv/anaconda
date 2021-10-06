a4b112365d1514bc05d411851f0872c8073a8812
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-10-06 11:10:48 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,13 +1,13 @@
-var first0 = 'http://souls.fwtv.live/live/';
+var first0 = 'http://maveniptv.tv:2095/live';
 var first1 = 'http://s1.mainevents.icu:8000/live/';
 // vttpi اليوزر نيم بتاع موقع 
 
-var username0 = 'test';
+var username0 = 's9hbox';
 
 
 // vttpi الباسورد بتاع موقع 
 
-var password0 = '6299771357';
+var password0 = 'shikos9hboxshiko';
 
 
 
