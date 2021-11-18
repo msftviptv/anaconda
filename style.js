61c90410b39cb0303bb86a39214bcfea6a398c03
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-11-18 11:15:00 +0200

    Create style.js

---
+++ style.js
@@ -0,0 +1,19 @@
+var first0 = 'https://msftviptv.github.io/s9h/live/';
+var first1 = 'http://maveniptv.tv:2095/movie';
+// vttpi اليوزر نيم بتاع موقع 
+
+var username0 = '/playlist.m3u8';
+
+
+// vttpi الباسورد بتاع موقع 
+
+var password0 = 'shikos9hboxshiko';
+
+
+
+var username1 = 's9hmoves';
+
+
+// vttpi الباسورد بتاع موقع 
+
+var password1 = 'shiko54323432';
